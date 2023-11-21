f5734d7b71a1c2e57381d30a925863d040679b02
TheRealChair <62349816+TheRealChair@users.noreply.github.com>
2023-11-21 16:18:16 +0100

    Created GUI class and most basic elements.

---
+++ src/main/java/com/group13/app/GUI_test.java
@@ -0,0 +1,34 @@
+package com.group13.app;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI_test implements ActionListener {
+
+    // The window and the panel.
+    JFrame frame;
+    JPanel panel;
+
+    // All the visual content of the product
+
+    public void GUI() {
+        // Creates the frame of the program.
+        frame = new JFrame();
+
+        // Creates the panel for all the content.
+        panel = new JPanel();
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
