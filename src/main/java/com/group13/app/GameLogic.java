1c4b23e25a28cb0a605a9a92db4e190544b911e9
TheRealChair <62349816+TheRealChair@users.noreply.github.com>
2023-11-24 11:44:46 +0100

    Translated board fields

--- src/main/java/com/group13/app/GameLogic.java
+++ src/main/java/com/group13/app/GameLogic.java
@@ -3,90 +3,78 @@ package com.group13.app;
 public abstract class GameLogic {
 
   final BoardSpace[] boardSpaces = {
-    new PassiveSpace("Start", "You passed the start!"),
-    new PropertySpace(
-      "Burgerbar",
-      "You've passed the Burgerbar!",
-      1,
-      Color.GREEN
-    ),
-    new PropertySpace(
-      "Pizzariaet",
-      "You've passed the Pizzaria!",
-      1,
-      Color.GREEN
-    ),
-    new ChanceSpace("Chance", "You've passed the Chance!"),
-    new PropertySpace(
-      "Slikbutikken",
-      "You've passed the Slikbutik!",
-      1,
-      Color.LIGHT_BLUE
-    ),
-    new PropertySpace(
-      "Iskiosken",
-      "You've passed the Iskiosk!",
-      1,
-      Color.LIGHT_BLUE
-    ),
-    new PassiveSpace("Fængselsbesøg", "You've passed the På besøg i fængsel!"),
-    new PropertySpace("Museet", "You've passed the Museum!", 2, Color.PINK),
-    new PropertySpace(
-      "Biblioteket",
-      "You've passed the Bibliotek!",
-      2,
-      Color.PINK
-    ),
-    new ChanceSpace("Chance", "You've passed the Chance!"),
-    new PropertySpace(
-      "Skaterparken",
-      "You've passed the Skaterpark!",
-      2,
-      Color.ORANGE
-    ),
-    new PropertySpace("Poolen", "You've passed the Pool!", 2, Color.ORANGE),
-    new PassiveSpace("Gratis parkering", "You've passed the Gratis parkering!"),
-    new PropertySpace(
-      "Spillehallen",
-      "You've passed the Spillehal!",
-      3,
-      Color.RED
-    ),
-    new PropertySpace("Biografen", "You've passed the Biograf!", 3, Color.RED),
-    new ChanceSpace("Chance", "You've passed the Chance!"),
-    new PropertySpace(
-      "Legetøjsbutikken",
-      "You've passed the Legetøjsbutik!",
-      3,
-      Color.YELLOW
-    ),
-    new PropertySpace(
-      "Dyrehandler",
-      "You've passed the Dyrehandler!",
-      3,
-      Color.YELLOW
-    ),
-    new JailSpace("Fængsel", "You've passed the Fængsel!"),
-    new PropertySpace(
-      "Bowlinghallen",
-      "You've passed the Bowlinghal!",
-      4,
-      Color.GREEN
-    ),
-    new PropertySpace("Zoo", "You've passed the Zoo!", 4, Color.GREEN),
-    new ChanceSpace("Chance", "You've passed the Chance!"),
-    new PropertySpace(
-      "Vandland",
-      "You've passed the Vandland!",
-      5,
-      Color.DARK_BLUE
-    ),
-    new PropertySpace(
-      "Strandpromenaden",
-      "You've passed the Strandpromenaden!",
-      5,
-      Color.DARK_BLUE
-    ),
+      new PassiveSpace("Start", "You passed the start!"),
+      new PropertySpace(
+          "Burger-Bar",
+          "You've passed the Burger-Bar!",
+          1,
+          Color.GREEN),
+      new PropertySpace(
+          "Pizzaria",
+          "You've passed the Pizzaria!",
+          1,
+          Color.GREEN),
+      new ChanceSpace("Chance", "You've passed the Chance!"),
+      new PropertySpace(
+          "Candy Store",
+          "You've passed the Candy Store!",
+          1,
+          Color.LIGHT_BLUE),
+      new PropertySpace(
+          "Icecream Stall",
+          "You've passed the Icecream Stall!",
+          1,
+          Color.LIGHT_BLUE),
+      new PassiveSpace("Jail Visit", "You're visting your uncle in Jail!"),
+      new PropertySpace("Museum", "You've passed the Museum!", 2, Color.PINK),
+      new PropertySpace(
+          "Library",
+          "You've passed the Library!",
+          2,
+          Color.PINK),
+      new ChanceSpace("Chance", "You've passed the Chance!"),
+      new PropertySpace(
+          "Skate Park",
+          "You've passed the Skate Park!",
+          2,
+          Color.ORANGE),
+      new PropertySpace("The Pool", "You've passed the Pool!", 2, Color.ORANGE),
+      new PassiveSpace("Free parking", "You've passed the Free parking!"),
+      new PropertySpace(
+          "Arcade",
+          "You've passed the Arcade!",
+          3,
+          Color.RED),
+      new PropertySpace("Cinema", "You've passed the Cinema!", 3, Color.RED),
+      new ChanceSpace("Chance", "You've passed the Chance!"),
+      new PropertySpace(
+          "Toy Store",
+          "You've passed the the Toy Store!",
+          3,
+          Color.YELLOW),
+      new PropertySpace(
+          "Pet Shop",
+          "You've passed the Pet Shop!",
+          3,
+          Color.YELLOW),
+      new JailSpace("Jail", "You're going straigt to Jail!"),
+      new PropertySpace(
+          "Bowling Alley",
+          "You've passed the Bowling Alley!",
+          4,
+          Color.GREEN),
+      new PropertySpace("Zoo", "You've passed the Zoo!", 4, Color.GREEN),
+      new ChanceSpace("Chance", "You've passed the Chance!"),
+      new PropertySpace(
+          "Water Park",
+          "You've passed the Water Park!",
+          5,
+          Color.DARK_BLUE),
+      new PropertySpace(
+          "The beach promenade",
+          "You've passed The beach promenade!",
+          5,
+          Color.DARK_BLUE),
   };
   protected Player[] players;
   protected int turn;
@@ -107,29 +95,26 @@ public abstract class GameLogic {
 
   private String usedGetOutOfJailFree(Player player) {
     return String.format(
-      "The player %s used a get out of jail free card and now has %d get out of jail cards left",
-      player.getName(),
-      player.getOutOfJailFree
-    );
+        "The player %s used a get out of jail free card and now has %d get out of jail cards left",
+        player.getName(),
+        player.getOutOfJailFree);
   }
 
   private String paidJail(Player player, BoardSpace space) {
     return String.format(
-      "The player %s paid the jail %d M and now has %d M left",
-      player.getName(),
-      JAIL_PENALTY,
-      player.account.getBalance()
-    );
+        "The player %s paid the jail %d M and now has %d M left",
+        player.getName(),
+        JAIL_PENALTY,
+        player.account.getBalance());
   }
 
   protected String wentBankrupt(Player player, BoardSpace space) {
     try {
       return String.format(
-        "The player %s went bankrupt when attempting to pay %d M on %s",
-        player.getName(),
-        space.getPrice(),
-        space.getName()
-      );
+          "The player %s went bankrupt when attempting to pay %d M on %s",
+          player.getName(),
+          space.getPrice(),
+          space.getName());
     } catch (Exception e) {
       return e.toString();
     }
@@ -138,12 +123,11 @@ public abstract class GameLogic {
   protected String boughtProperty(Player player, BoardSpace space) {
     try {
       return String.format(
-        "The player %s bought the property %s for %d M and now has %d M left",
-        player.getName(),
-        space.getName(),
-        space.getPrice(),
-        player.account.getBalance()
-      );
+          "The player %s bought the property %s for %d M and now has %d M left",
+          player.getName(),
+          space.getName(),
+          space.getPrice(),
+          player.account.getBalance());
     } catch (Exception e) {
       return e.toString();
     }
@@ -152,12 +136,11 @@ public abstract class GameLogic {
   protected String paidRentToProperty(Player player, BoardSpace space) {
     try {
       return String.format(
-        "The player %s paid %d M in rent on the space %s and now has %d M left",
-        player.getName(),
-        space.getPrice(),
-        space.getName(),
-        player.account.getBalance()
-      );
+          "The player %s paid %d M in rent on the space %s and now has %d M left",
+          player.getName(),
+          space.getPrice(),
+          space.getName(),
+          player.account.getBalance());
     } catch (Exception e) {
       return e.toString();
     }
@@ -165,29 +148,26 @@ public abstract class GameLogic {
 
   protected String playerRolled(Player player, BoardSpace space) {
     return String.format(
-      "The player %s rolled a %d and landed on position %d, %s",
-      players[turn].getName(),
-      dice.getValue(),
-      players[turn].getPosition(),
-      space.getName()
-    );
+        "The player %s rolled a %d and landed on position %d, %s",
+        players[turn].getName(),
+        dice.getValue(),
+        players[turn].getPosition(),
+        space.getName());
   }
 
   protected String playerPassedStart(Player player) {
     return String.format(
-      "The player %s passed the start and recieved %d M and now has %d M",
-      players[turn].getName(),
-      PASS_START_REWARD,
-      players[turn].account.getBalance()
-    );
+        "The player %s passed the start and recieved %d M and now has %d M",
+        players[turn].getName(),
+        PASS_START_REWARD,
+        players[turn].account.getBalance());
   }
 
   protected String winnerIs(Player player) {
     return String.format(
-      "The winner is %s who had %d M left",
-      players[turn].getName(),
-      players[turn].account.getBalance()
-    );
+        "The winner is %s who had %d M left",
+        players[turn].getName(),
+        players[turn].account.getBalance());
   }
 
   protected abstract void displayMessage(String message);
@@ -200,8 +180,7 @@ public abstract class GameLogic {
     initPlayers();
     while (isPlaying) {
       System.out.print(
-        "Turn: " + players[turn].getName() + ", roll (r), quit (q):"
-      );
+          "Turn: " + players[turn].getName() + ", roll (r), quit (q):");
       var action = getUserInput();
       if (action.equals("r")) {
         playerRollDice();
@@ -221,7 +200,8 @@ public abstract class GameLogic {
       try {
         var userInput = getUserInput();
         playerAmount = Integer.parseInt(userInput);
-        if (playerAmount < 2 || 4 < playerAmount) throw new Exception();
+        if (playerAmount < 2 || 4 < playerAmount)
+          throw new Exception();
         break;
       } catch (Exception e) {
         System.out.println("Invalid input");
@@ -237,12 +217,10 @@ public abstract class GameLogic {
       startBalance = 16;
     }
     System.out.println(
-      String.format(
-        "The number of players is %d which means the start balance is %d M",
-        playerAmount,
-        startBalance
-      )
-    );
+        String.format(
+            "The number of players is %d which means the start balance is %d M",
+            playerAmount,
+            startBalance));
     for (var i = 0; i < players.length; i++) {
       System.out.print("What is the name of player " + (i + 1) + "? ");
       var name = getUserInput();
@@ -283,8 +261,7 @@ public abstract class GameLogic {
             space.setOwner(players[turn]);
             displayMessage(boughtProperty(players[turn], space));
           } else {
-            var paidPlayer =
-              players[turn].giveMoney(space.getOwner(), space.getPrice());
+            var paidPlayer = players[turn].giveMoney(space.getOwner(), space.getPrice());
             if (!paidPlayer) {
               stopGame(space);
               return;
@@ -320,10 +297,10 @@ public abstract class GameLogic {
             try {
               var userInput = getUserInput();
               var userInputInt = Integer.parseInt(userInput);
-              if (userInputInt < 1 || 5 < userInputInt) throw new Exception();
+              if (userInputInt < 1 || 5 < userInputInt)
+                throw new Exception();
               players[turn].setPosition(
-                  userInputInt + players[turn].getPosition()
-                );
+                  userInputInt + players[turn].getPosition());
               playerLandedOnSpace(space);
               break;
             } catch (Exception e) {
@@ -340,11 +317,10 @@ public abstract class GameLogic {
         case 6:
           bank.takeMoney(players[turn], 2);
           displayMessage(
-            "The player" +
-            players[turn].getName() +
-            " has recieved 2M and now has " +
-            players[turn].account.getBalance()
-          );
+              "The player" +
+                  players[turn].getName() +
+                  " has recieved 2M and now has " +
+                  players[turn].account.getBalance());
           break;
         case 7:
           break;
@@ -382,8 +358,7 @@ public abstract class GameLogic {
             } else {
               players[turn].giveMoney(
                   boardSpaces[10].getOwner(),
-                  boardSpaces[10].getPrice()
-                );
+                  boardSpaces[10].getPrice());
             }
           } catch (Exception e) {
             // TODO: handle exception
@@ -396,7 +371,8 @@ public abstract class GameLogic {
         default:
           break;
       }
-    } else if (space instanceof PassiveSpace) {} else {
+    } else if (space instanceof PassiveSpace) {
+    } else {
       System.out.println("Error: space type not recognized");
     }
   }
@@ -407,8 +383,7 @@ public abstract class GameLogic {
       bank.giveMoney(players[turn], PASS_START_REWARD);
       displayMessage(playerPassedStart(players[turn]));
     }
-    var newPosition =
-      (players[turn].getPosition() + dice.getValue()) % boardSpaces.length;
+    var newPosition = (players[turn].getPosition() + dice.getValue()) % boardSpaces.length;
     players[turn].setPosition(newPosition);
 
     var space = boardSpaces[newPosition];
@@ -428,9 +403,7 @@ public abstract class GameLogic {
     while (!sorted) {
       sorted = true;
       for (var i = 0; i < players.length - 1; i++) {
-        if (
-          players[i].account.getBalance() < players[i + 1].account.getBalance()
-        ) {
+        if (players[i].account.getBalance() < players[i + 1].account.getBalance()) {
           var temp = players[i];
           players[i] = players[i + 1];
           players[i + 1] = temp;
@@ -459,8 +432,7 @@ public abstract class GameLogic {
     sortPlayersByBalance();
     var winners = "";
     for (var i = 0; i < players.length; i++) {
-      winners +=
-        (i + 1) + ": " + players[i].toString() + System.lineSeparator();
+      winners += (i + 1) + ": " + players[i].toString() + System.lineSeparator();
     }
     return winners;
   }
